f157fcce461810d12c5db1e8042c5413c29df69d
Austin Christ <christ.austinw@gmail.com>
2018-01-08 20:09:54 -0700

    Move description text to code
    Move the text definition for the example card to code and utilize Kotlin android extensions to avoid boiler plate code for setting up the TextView

--- app/src/main/java/com/christaustin/habittrainer/MainActivity.kt
+++ app/src/main/java/com/christaustin/habittrainer/MainActivity.kt
@@ -2,11 +2,14 @@ package com.christaustin.habittrainer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_description.text = "A refreshing glass of water gets you hydrated"
     }
 }
