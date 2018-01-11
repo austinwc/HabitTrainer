f9717a2cfa6169865e57ad0fda12922130f08b2d
Austin Christ <maximuscato@gmail.com>
2018-01-10 21:05:51 -0700

    Remove string literals from XML
    Create resources for strings and assign values in OnCreate using Kotlin
    android extensions.
    
    Signed-off-by: Austin Christ <maximuscato@gmail.com>

--- app/src/main/java/com/christaustin/habittrainer/MainActivity.kt
+++ app/src/main/java/com/christaustin/habittrainer/MainActivity.kt
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tv_description.text = "A refreshing glass of water gets you hydrated"
+        tv_title.text = getString(R.string.drink_water_title)
+        iv_icon.setImageResource(R.drawable.water)
+        tv_description.text = getString(R.string.drink_water_description)
     }
 }
