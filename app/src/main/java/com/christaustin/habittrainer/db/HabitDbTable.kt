3c72e885970776c81749f99171c74e521a23ac2a
Austin Christ <maximuscato@gmail.com>
2018-01-21 12:13:37 -0700

    Add transaction extension function to SQLiteDatabase
    The transaction boiler-plate code that is necessary for each transaction
    with the Android SQLiteDatabase class has been modularized into an
    extension function to the class. This extension function takes an argument
    that is a extension function lambda and executes that function with the
    necessary transaction handling (in this case the extension function
    lambda is insert() but it could easily be delete() or any other DB
    operation).
    
    The function is made more generic by handling generic type T allowing
    for the lambda passed to transaction() to return its own type of return
    value. In the example this is ID which is a long, but it could
    easily be any other return type including Unit (a Kotlin place holder for
    no return type).
    
    Finally, the function is declared inline which avoids overhead of making
    anonymous classes for the lambda function at execution time. We essentially
    gain all the coding benefit of a robust and generic extension function
    that aids in code accuracy and readability without taking a performance
    penalty.
    
    Signed-off-by: Austin Christ <maximuscato@gmail.com>

--- app/src/main/java/com/christaustin/habittrainer/db/HabitDbTable.kt
+++ app/src/main/java/com/christaustin/habittrainer/db/HabitDbTable.kt
@@ -2,13 +2,14 @@ package com.christaustin.habittrainer.db
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import android.graphics.Bitmap
 import android.util.Log
 import com.christaustin.habittrainer.Habit
 import java.io.ByteArrayOutputStream
 
 /**
- * Created by maximuscato on 1/21/18.
+ * Class to handle database table
  */
 class HabitDbTable(context: Context) {
     private val TAG = HabitDbTable::class.java.simpleName
@@ -22,16 +23,9 @@ class HabitDbTable(context: Context) {
         values.put(HabitEntry.DESCR_COL, habit.description)
         values.put(HabitEntry.IMAGE_COL, toByteArray(habit.image))
 
-        db.beginTransaction()
-        val id = try {
-            val returnValue = db.insert(HabitEntry.TABLE_NAME, null, values)
-            db.setTransactionSuccessful()
-
-            returnValue
-        } finally {
-            db.endTransaction()
+        val id = db.transaction {
+            insert(HabitEntry.TABLE_NAME, null, values)
         }
-        db.close()
 
         Log.d(TAG, "Stored a new habit to the DB $habit")
 
@@ -44,4 +38,19 @@ class HabitDbTable(context: Context) {
         bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream)
         return stream.toByteArray()
     }
+}
+
+private inline fun <T> SQLiteDatabase.transaction(function: SQLiteDatabase.() -> T): T {
+    beginTransaction()
+    val result = try {
+        val returnValue = function()
+        setTransactionSuccessful()
+
+        returnValue
+    } finally {
+        endTransaction()
+    }
+    close()
+
+    return result
 }
